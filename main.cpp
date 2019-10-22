ad98abb707afd7a416060e9b98eeba845487617b
RKRohk <rkakar2000@gmail.com>
2019-10-22 22:32:35 +0530

    Changed stuff

--- main.cpp
+++ main.cpp
@@ -3,22 +3,46 @@
 #include<string>
 using namespace std;
 class Student{
+    protected:
     string name,subject[9] = {"Probability and Statistics","Discrete Mathematics","Economics","Object Oriented Programming","Object Oriented Programming Lab","Data Structures and Algorithms","Data Structures and Algorithms lab","Principle of Digital Communication"};
-    float marks[8],sgpa,cgpa,credits[8];
+    float marks,sgpa,cgpa,credits[8];
     public:
     void getInfo(){
-        for(int i=0;i<8;i++){
-            cout<<"Enter marks for subject "<<subject[i]<<endl;
-            cin>>marks[i];
-        }
+        cout<<"Enter name"<<endl;
+        getline(cin,name);
+        cout<<"Enter marks"<<endl; //This has to be converted to loop, I just did this to try it
+        cin>>marks; 
     }
     void calcSgpa(){
         //TODO I really don't know how to calculate sgpa. HALP!
     }
-    void calcSgpa(){
+    void calcCgpa(){
         cgpa = (cgpa*2 + sgpa)/3.0;
     }
-    string retName(){
-        return name;
+    void show(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"MARKS:"<<marks<<endl;
     }
 };
+int main(){
+    ofstream ofile;
+    ofile.open("StudentsTest.txt",ios::app);    
+    int ch=1;
+    while (ch)
+    {
+        Student s;
+        s.getInfo();
+        s.show();
+        ofile.write((char *) & s, sizeof(s));
+        cout<<"Want to enter more data? Y/N"<<endl;
+        cin>>ch;
+    }    
+    ifstream ifile("StudentsTest.txt",ios::in);
+    Student stud;
+    ifile.read((char *)&stud,sizeof(stud));
+    stud.show();
+    ofile.close();
+    ifile.close();
+    remove("StudentsTest.txt");
+    return 0;
+}
